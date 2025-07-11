91b6c01d93b4e3f169632ff847883642ee6f9c07
devops542 <rakeshdevops542@gmail.com>
2025-07-11 09:31:07 +0000

    addede new line

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
     System.out.println("Hello, World");
-  }
+ today is friday and it is the 5th day
+ tomorrow is saturday
+ : }
 }
